90e4cd6e5dc4d7996023e3361c4829863f89383e
Carlos <hen9@ymail.com>
2025-06-27 14:26:07 -0300

    corrigindo script

--- config/tables.sql
+++ config/tables.sql
@@ -1,34 +1,27 @@
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `pessoa`
---
-
-CREATE TABLE `pessoa` (
-  `id` int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  `nome` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `login` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
-  `endereco` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
-  `telefone` varchar(35) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
-  `sexo` varchar(1) COLLATE utf8_unicode_ci NOT NULL,
-  `senha` VARCHAR(255) NOT NULL
+-- Tabela: pessoa
+CREATE TABLE pessoa (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(50) COLLATE utf8_unicode_ci NOT NULL,
+  login VARCHAR(80) COLLATE utf8_unicode_ci NOT NULL,
+  endereco VARCHAR(80) COLLATE utf8_unicode_ci NOT NULL,
+  telefone VARCHAR(35) COLLATE utf8_unicode_ci NOT NULL,
+  email VARCHAR(40) COLLATE utf8_unicode_ci NOT NULL,
+  sexo VARCHAR(1) COLLATE utf8_unicode_ci NOT NULL,
+  senha VARCHAR(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Estrutura da tabela `noticia`
---
-
-CREATE TABLE `noticia` (
-  `id` int(11) AUTO_INCREMENT PRIMARY KEY,
-  `titulo` varchar(50)   NOT NULL,
-  `texto` TEXT   NOT NULL,
-  `tag1` varchar(10),
-  `tag2` varchar(10),
-  `tag3` varchar(10),
-  `id_autor` int(11) ,
-  `foto`   LONGBLOB,
-  `data_criacao` TIMESTAMP
+-- Tabela: noticia
+CREATE TABLE noticia (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  titulo VARCHAR(50) NOT NULL,
+  texto TEXT NOT NULL,
+  tag1 VARCHAR(10),
+  tag2 VARCHAR(10),
+  tag3 VARCHAR(10),
+  id_autor INT UNSIGNED,
+  foto LONGBLOB,
+  data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT fk_noticia_autor FOREIGN KEY (id_autor) REFERENCES pessoa(id)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
- 
-ALTER TABLE noticia ADD CONSTRAINT fk_id_autor FOREIGN KEY ( id_autor ) REFERENCES pessoa ( id ) ;
