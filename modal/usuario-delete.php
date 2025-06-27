4a9546eb265e0b92fe2b8262291f7fd81bd5bc6a
Carlos <hen9@ymail.com>
2025-06-27 11:21:12 -0300

    melhoria da mensagem de sucesso

--- modal/usuario-delete.php
+++ modal/usuario-delete.php
@@ -3,18 +3,14 @@ require_once __DIR__ . '/../DAO/usuarios.php';
 require_once __DIR__ . '/../DAO/noticia.php';
 
 $id = 0;
-
 if (!empty($_GET['id'])) {
     $id = $_REQUEST['id'];
-
-
     $noticiaService = new NoticiaService();
     $noticiaQTD = (int) $noticiaService->findByIdUsuario($id);
-
-
     if ($noticiaQTD == 0) {
         $usuarioService = new UsuarioService();
         $usuarioService->deleteById($id);
+        echo 'deletado user_id'.$id;
     } else {
         echo 'Usuáro tem noticias associadas!';
     }
